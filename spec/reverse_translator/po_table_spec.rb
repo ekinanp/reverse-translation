eb844f20e57f540a9c01767bbfeea425795aecce
Enis Inan <enis.inan@enis.inan-C02M48DGFD57>
2017-08-13 14:22:48 -0700

    Strengthened existing tests + minor refactoring of tests. Also removed old files.

--- spec/reverse_translator/po_table_spec.rb
+++ spec/reverse_translator/po_table_spec.rb
@@ -1,18 +1,16 @@
 require "spec_helper"
 
 describe POTable do
-  before do
-    allow(POParser).to receive(:parse).with(POTableFixture::MOCK_PO_FILE).and_return(POTableFixture::MOCK_PO_PARSER_DATA)
-    @data_entry_map = POTableFixture::MOCK_PO_PARSER_DATA.map { |d| [d, double()] }.to_h
-    @data_entry_map.each do |d, e| 
-      allow(e).to receive(:reverse_translate) { |msg| msg + (POTableFixture::TRANSLATIONS[d]) } 
-    end
-    allow(POEntry).to receive(:new) { |d| @data_entry_map[d] }
-    @table = POTable.new (POTableFixture::MOCK_PO_FILE)
-  end
-
   describe "#reverse_translate" do
     it "should translate the given message starting with the longest translations" do
+      allow(POParser).to receive(:parse).with(POTableFixture::MOCK_PO_FILE).once.and_return(POTableFixture::MOCK_PO_PARSER_DATA)
+      @data_entry_map = POTableFixture::MOCK_PO_PARSER_DATA.map { |d| [d, double()] }.to_h
+      @data_entry_map.each do |d, e| 
+        allow(e).to receive(:reverse_translate).once { |msg| msg + (POTableFixture::TRANSLATIONS[d]) } 
+      end
+      allow(POEntry).to receive(:new).exactly(@data_entry_map.size).times { |d| @data_entry_map[d] }
+      @table = POTable.new (POTableFixture::MOCK_PO_FILE)
+
       expect(@table.reverse_translate("")).to eql("CBA")
     end
   end
--- spec/reverse_translator/reverse_translator_spec.rb
+++ spec/reverse_translator/reverse_translator_spec.rb
@@ -1,30 +1,28 @@
 require "spec_helper"
 
 describe ReverseTranslator do
-  before do
-    @data_table_map = ReverseTranslatorFixture::MOCK_PO_FILES.map { |d| [d, double()] }.to_h
-    @data_table_map.each do |d, t| 
-      allow(t).to receive(:reverse_translate) { |msg| msg + (ReverseTranslatorFixture::TRANSLATIONS[d]) } 
-    end
-    allow(POTable).to receive(:new) { |d| @data_table_map[d] }
-
-    allow(LogParser).to receive(:parse).with(ReverseTranslatorFixture::MOCK_LOG_FILE).and_return(ReverseTranslatorFixture::MOCK_LOG_MSGS_MAP.keys)
-
-    @out_file = double()
-    @translated_log = ""
-    allow(@out_file).to receive(:puts) { |msg| @translated_log = @translated_log + msg + "\n" }
-    allow(File).to receive(:open).with(ReverseTranslatorFixture::MOCK_LOG_FILE+".trans", "w").and_return(@out_file)
-    @file_closed = false
-    allow(@out_file).to receive(:close).with(no_args) { @file_closed = true }
-
-    @reverse_translator = ReverseTranslator.new (ReverseTranslatorFixture::MOCK_PO_FILES)
-  end
-
   describe "#reverse_translate" do
     it "should translate the given log file (denoted by <path>), with the translated log file's path being <path>.trans" do
+      num_log_msgs = ReverseTranslatorFixture::MOCK_LOG_MSGS_MAP.size
+      num_po_files = ReverseTranslatorFixture::MOCK_PO_FILES.size
+  
+      @data_table_map = ReverseTranslatorFixture::MOCK_PO_FILES.map { |d| [d, double()] }.to_h
+      @data_table_map.each do |d, t| 
+        allow(t).to receive(:reverse_translate).exactly(num_log_msgs).times { |msg| msg + (ReverseTranslatorFixture::TRANSLATIONS[d]) } 
+      end
+      allow(POTable).to receive(:new).exactly(num_po_files).times { |d| @data_table_map[d] }
+  
+      allow(LogParser).to receive(:parse).with(ReverseTranslatorFixture::MOCK_LOG_FILE).once.and_return(ReverseTranslatorFixture::MOCK_LOG_MSGS_MAP.keys)
+  
+      @out_file = double()
+      @translated_log = ""
+      allow(@out_file).to receive(:puts).exactly(num_log_msgs).times { |msg| @translated_log = @translated_log + msg + "\n" }
+      allow(File).to receive(:open).with(ReverseTranslatorFixture::MOCK_LOG_FILE+".trans", "w").once.and_return(@out_file)
+      expect(@out_file).to receive(:close).with(no_args).once 
+  
+      @reverse_translator = ReverseTranslator.new (ReverseTranslatorFixture::MOCK_PO_FILES)
       @reverse_translator.reverse_translate(ReverseTranslatorFixture::MOCK_LOG_FILE)
       expect(@translated_log).to eql(ReverseTranslatorFixture::EXPECTED_TRANSLATED_LOG)
-      expect(@file_closed).to be true
     end
   end
 end
