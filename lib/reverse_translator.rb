ceb1e622cac2e694ff6b73cd630b7aecc4e3d42e
Enis Inan <enis.inan@enis.inan-C02M48DGFD57>
2017-08-13 14:02:16 -0700

    Added tests for ReverseTranslator.

--- lib/reverse_translator.rb
+++ lib/reverse_translator.rb
@@ -9,27 +9,17 @@ require_relative 'log_parser'
 # encapsulates a single POT file. For initialization, it will take the list of
 # POT files that it needs in order to create its lookup tables.
 class ReverseTranslator 
-  attr_reader :tables 
-
   def initialize(pot_files)
     @tables = pot_files.map { |file| POTable.new(file) }
   end
 
-  # TODO: This method should have a REGEX denoting a valid prefix
-  # for the log file. Either this can be a class-level constant,
-  # or it can be provided as an input parameter into this function.
-  # For now, we will assume that the log file consists only of messages.
-  # What if a LOG spans more than one line? The prefixes will separate
-  # various LOG files. So I need to add that here. This needs a lot more
-  # work in order for it to do its job.
-  #
   # Reverse translations will be written with the ".trans" extension.
   # This can be changed in the future
   def reverse_translate(log_file)
     log_file_trans = log_file + ".trans"
     out_file = File.open(log_file_trans, "w")
     LogParser.parse(log_file).each do |entry|
-      translated_entry = tables.inject(entry) do |new_entry, table|
+      translated_entry = @tables.inject(entry) do |new_entry, table|
         table.reverse_translate(new_entry)
       end
       out_file.puts(translated_entry)
