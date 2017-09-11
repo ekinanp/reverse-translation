a49e073930cc8969e35723639787db8b1f8d3016
Enis Inan <enis.inan@enis.inan-C02M48DGFD57>
2017-09-11 00:59:46 -0700

    Setup framework for ParameterizedString tests.

---
+++ spec/fixtures/parameterized_string_fixture.rb
@@ -0,0 +1,143 @@
+module ParameterizedStringFixture
+  # Testing the individual parameter regexes. These are the data
+  # that'll be used for the happy cases.
+  IDENTIFIERS = ["_foo", "foo", "foo12345", "foo123__foo123"]
+  INTEGERS = (1..10)
+
+  PRINTF_INDEX = INTEGERS.map { |i| "#{i}$" }
+  PRINTF_FLAGS = "-+ 0#".split("")
+  PRINTF_WIDTHS = INTEGERS.map { |i| i.to_s }
+  PRINTF_PRECISION = INTEGERS.map { |i| ".#{i}" } 
+  PRINTF_LENGTHS = ["hh", "ll", "h", "l", "L", "z", "j", "t"]
+  PRINTF_TYPES = "diufFeEgGxXoscpaAn".split("")
+
+  # Some bad input that should not be matched.
+  STANDARD_BAD_INPUT = ["{}", "{0", "0}", "{0 }", "{ 0}", "{ 0 }", ""]
+  RUBY_PERCENT_BAD_INPUT = [
+    "%{}", 
+    "{foo}", 
+    "%{foo", 
+    "foo}", 
+    "%{foo }", 
+    "%{ foo}",
+    "%{ foo }",
+    "%{1foo}",
+    "%{234foo}",
+    "%{foo!@#!@}",
+    ""
+  ]
+  HANDLEBAR_BAD_INPUT = [
+    "{{}}", 
+    "{foo}", 
+    "{{foo", 
+    "foo}}", 
+    "{foo}}",
+    "{{foo}",
+    "{{foo }}", 
+    "{{ foo}}",
+    "{{ foo }}",
+    "{{1foo}}",
+    "{{234foo}}",
+    "{{foo!@#!@}}"
+  ]
+
+  # printf is trickier, as the regex is complex. It is best to test the following:
+  #   (1) Ensure that the individual pieces (INDEX, FLAGS, WIDTH) do not match
+  #   their respective bad inputs.
+  #
+  #   (2) Ensure that some combination of the individual pieces does not match the
+  #   main PRINTF regex.
+  INDEX_BAD_INPUT = ["$", "abc$", "$1", "1 $", "$ 1"]
+  FLAGS_BAD_INPUT = ["%", "^", "*", "@", "_", "r"]
+  WIDTH_BAD_INPUT = ["abc", "abc1234"]
+  PRECISION_BAD_INPUT = ["1", "123.", ".abc", "abc."]
+  LENGTH_BAD_INPUT = ["aa" ,"bb" ,"ee", "11", "y"]
+  TYPE_BAD_INPUT = ["z", "w", "y", "v", "q", ";", ""] 
+  PRINTF_BAD_INPUT = [
+    "%%",
+    "%%s",
+    "s",
+    "%m",
+    "%zzu",
+    "%$11.23s",
+    "%$1llu",
+    "%1$z",
+    "%1+1llu",
+    "%1$^hhd",
+    ""
+  ]
+
+  # Map of regex to its bad input cases for the various components that
+  # make up the PRINTF regex.
+  PRINTF_BAD_INPUT_CASES = {
+    ParameterizedString::INDEX     => INDEX_BAD_INPUT,
+    ParameterizedString::FLAGS     => FLAGS_BAD_INPUT,
+    ParameterizedString::WIDTH     => WIDTH_BAD_INPUT,
+    ParameterizedString::PRECISION => PRECISION_BAD_INPUT,
+    ParameterizedString::LENGTH    => LENGTH_BAD_INPUT,
+    ParameterizedString::TYPE      => TYPE_BAD_INPUT,
+    ParameterizedString::PRINTF    => PRINTF_BAD_INPUT
+  }
+
+  # Aliases to the regexes, to save typing
+  STANDARD = ParameterizedString::STANDARD
+  PRINTF = ParameterizedString::PRINTF
+
+  # All of the test cases below will have three pieces to them:
+  #    [param_str, param_re, other]
+  # where param_str and param_re are what will be used to construct the parameterized
+  # string object, and other is another necessary component for that specific test case.
+  # This can be either an additional input, an expected value, whatever. But
+  # the idea is that each test case will be built from the param_str's overall structure,
+  # given by the <PREFIX>(<PARAM><MID>)* regex described in the ParameterizedString class'
+  # comments. Here, however, <PARAM> is represented as a hash map of <id> => <original value>two key components:
+  # where <original value> is how the param. appears in the parameterized string (e.g.
+  # "%s" for printf, %{<identifier>} for a ruby percent parameter, etc.
+  #
+  # NOTE: There might be some redundant test cases for some of the parameterized string class'
+  # methods, but this abstraction makes it simpler to add a new test case capturing a larger
+  # class of instance methods because both methods will use the same pieces.
+  TEST_CASE_PIECES = [
+    ["This string has no parameters", [], STANDARD],
+
+    # For any regex other than PRINTF
+    ["This ", [[{"0" => "{0}"}, " has "], [{"1" => "{1}"}, " parameter"]], STANDARD],
+    ["", [[{"0" => "{0}"}, " has "], [{"1" => "{1}"}, " parameter"]], STANDARD],
+    ["", [[{"0" => "{0}"}, " has "], [{"1" => "{1}"}, " parameter for "], [{"2" => "{2}"}, ""]], STANDARD],
+    ["", [[{"0" => "{0}"}, " has "], [{"0" => "{0}"}, " at "], [{"1" => "{1}"}, " and "], [{"1" => "{1}"}, ""]], STANDARD],
+
+    # For PRINTF, but with non-positional parameters
+    ["This ", [[{"1" => "%s"}, " has "], [{"2" => "%d"}, " parameter"]], PRINTF],
+    ["", [[{"1" => "%s"}, " has "], [{"2" => "%d"}, " parameter"]], PRINTF],
+    ["", [[{"1" => "%s"}, " has "], [{"2" => "%s"}, " at "], [{"3" => "%d"}, " and "], [{"4" => "%d"}, ""]], PRINTF],
+
+    # For PRINTF, but with positional parameters
+    ["This ", [[{"1" => "%1$s"}, " has "], [{"2" => "%2$d"}, " parameter"]], PRINTF],
+    ["", [[{"1" => "%1$s"}, " has "], [{"2" => "%2$d"}, " parameter"]], PRINTF],
+    ["", [[{"1" => "%1$s"}, " has "], [{"2" => "%2$d"}, " parameter for "], [{"3" => "%3$f"}, ""]], PRINTF],
+    ["", [[{"1" => "%1$s"}, " has "], [{"1" => "%1$s"}, " at "], [{"2" => "%2$d"}, " and "], [{"2" => "%2$d"}, ""]], PRINTF]
+  ]
+
+  # This method builds a test case using the above test case pieces. It takes in a block
+  # that specifies the "other" part outlined above. For substitute_values, these will be
+  # the values that will be used to pass in to that routine; for params, it will be the
+  # expected order of the parameters, etc.
+  def self.construct_test_case
+    TEST_CASE_PIECES.map do |(prefix, param_mids, param_re)|
+      param_str = param_mids.inject(prefix) do |accum, (param, mid)|
+        accum + param.values[0] + mid
+      end
+      [param_str, param_re, *yield(prefix, param_mids, param_re)]
+    end
+  end
+
+  SUBSTITUTE_VALUES_TEST_CASES = construct_test_case do |prefix, param_mids, param_re|
+    [param_mids.inject({}) do |accum, (param, mid)|
+      accum.merge(param)
+    end]
+  end
+
+  PARAMS_TEST_CASES = construct_test_case do |prefix, param_mids, param_re|
+    [param_mids.map { |(param, mid)| param.keys[0] }.uniq ]
+  end
+end
---
+++ spec/reverse_translator/parameterized_string_spec.rb
@@ -0,0 +1,133 @@
+require "spec_helper"
+
+describe ParameterizedString do
+  # Regexes 
+  STANDARD = ParameterizedString::STANDARD
+  RUBY_PERCENT = ParameterizedString::RUBY_PERCENT
+  HANDLEBAR = ParameterizedString::HANDLEBAR
+  PRINTF = ParameterizedString::PRINTF 
+  PARAM_RES = ParameterizedString::PARAM_RES
+
+  # Input data for happy cases
+  IDENTIFIERS = ParameterizedStringFixture::IDENTIFIERS
+  INTEGERS = ParameterizedStringFixture::INTEGERS
+
+  # Regex tests
+  describe "regex describing standard parameters" do
+    context "given a valid parameter" do
+      it "matches it" do
+        INTEGERS.each { |i| expect("{#{i}}").to match(STANDARD) } 
+      end
+    end
+    context "given an invalid parameter" do
+      it "does not match it" do
+        ParameterizedStringFixture::STANDARD_BAD_INPUT.each { |s| expect(s).not_to match(STANDARD) }
+      end
+    end
+  end
+
+  describe "regex describing parameters in ruby-percent style" do
+    context "given a valid parameter" do
+      it "matches it" do
+        IDENTIFIERS.each { |i| expect("%{#{i}}").to match(RUBY_PERCENT) } 
+      end
+    end
+    context "given an invalid parameter" do
+      it "does not match it" do
+        ParameterizedStringFixture::RUBY_PERCENT_BAD_INPUT.each { |s| expect(s).not_to match(RUBY_PERCENT) }
+      end
+    end
+  end
+
+  describe "regex describing parameters using handlebar notation" do
+    context "given a valid parameter" do
+      it "matches it" do
+        IDENTIFIERS.each { |i| expect("{{#{i}}}").to match(HANDLEBAR) } 
+      end
+    end
+    context "given an invalid parameter" do
+      it "does not match it" do
+        ParameterizedStringFixture::HANDLEBAR_BAD_INPUT.each { |s| expect(s).not_to match(HANDLEBAR) }
+      end
+    end
+  end
+
+  describe "regex describing printf-style parameters" do
+    context "given a valid parameter" do
+      # Exhaustive testing here. If we denote the following sets:
+      #    A = INDEX
+      #    B = FLAGS
+      #    C = WIDTHS
+      #    D = PRECISION
+      #    E = LENGTHS
+      #    F = TYPES
+      # Then note that only Set F is required. Let x = xAxBxCxDxE be
+      # a 5-bit binary number, where xY represents whether Set Y is
+      # included or not. Assume that x = 10101. Then the number of
+      # possible valid printf parameters for this state (using a finite
+      # subset of the integers) is thus |xA||xC||xE||xF|. Taking into
+      # consideration all possible values of x (2^5 = 32), the total
+      # number of possible test cases that will be considered here is
+      # bounded above by 32|xA||xB||xC||xD||xE||xF| = 576000.
+      it "matches it" do
+        OPTIONAL_SETS = [
+          ParameterizedStringFixture::PRINTF_INDEX,
+          ParameterizedStringFixture::PRINTF_FLAGS,
+          ParameterizedStringFixture::PRINTF_WIDTHS,
+          ParameterizedStringFixture::PRINTF_PRECISION,
+          ParameterizedStringFixture::PRINTF_LENGTHS,
+        ]
+        REQUIRED_SET = ParameterizedStringFixture::PRINTF_TYPES
+        (0..31).each do |n|
+          # Find the included sets and reverse the result so that we can
+          # create strings by prepending to the front of the string instead
+          # of the end.
+          sets = OPTIONAL_SETS.each_with_index.inject([]) do |accum, (set, i)|
+            next accum if (n & (1 << (4 - i))) == 0
+            accum << set
+          end.reverse 
+
+          # Compute the input cases, which is the concatenated Cartesian product
+          # of all the included sets prepended with a "%" sign, and then see if
+          # they match the regex.
+          inputs = sets.inject(REQUIRED_SET) do |accum, set|
+            set.product(accum).map { |p| p.join }
+          end.map { |i| "%#{i}" }
+          inputs.each { |i| expect(i).to match(PRINTF) }
+        end
+      end
+    end
+    context "given an invalid parameter" do
+      it "does not match it" do
+        ParameterizedStringFixture::PRINTF_BAD_INPUT_CASES.each do |re, bad_inputs|
+          bad_inputs.each do |input|
+            # Optional regexes must match only the empty string. Non-optional ones
+            # cannot match.
+            next expect(re.match(input)[0]).to eql("") if re.match("")
+            expect(input).not_to match(re)
+          end
+        end
+      end
+    end
+  end
+
+  describe "#substitute_values" do
+    context "when the parameterized string is passed in a param_str and param_re for initialization" do
+      it "enforces the property that ps.substitute_values(param_vals) == param_str where param_vals maps the param IDs to their original representation in param_str" do
+        ParameterizedStringFixture::SUBSTITUTE_VALUES_TEST_CASES.each do |(param_str, param_re, param_vals)|
+          ps = ParameterizedString.new(param_str, param_re)
+          expect(ps.substitute_values(param_vals)).to eql(param_str)
+        end
+      end
+    end
+  end
+
+  describe "#params" do
+    it "returns an array of all the unique parameters appearing in the parameterized string" do
+      ParameterizedStringFixture::PARAMS_TEST_CASES.each do |(param_str, param_re, expected)|
+        ps = ParameterizedString.new(param_str, param_re)
+        expect(ps.params).to eql(expected)
+      end
+    end
+  end
+end
